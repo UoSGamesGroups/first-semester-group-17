1c6543ee6774493734633293d53ab5e916c33c4f
RazvanM23 <S175977@ucs.ac.uk>
2016-10-22 10:41:43 +0100

    Unity Scene for BoredomWarrior
    Using placeholder art it's a basic Unity scene and it has 4 scripts.
    Collectibles, movement, score and shooting.

---
+++ Boredom Warrior - Unity Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float moveSpeed;
+	public float jumpHeight;
+	public float groundCheckRadius;
+	public Transform groundCheck; 
+	public LayerMask whatIsGround; 
+	private bool grounded; 
+	private bool doubleJump; 
+	private float moveVelocity; 
+	public Transform firePoint;
+	public GameObject sword;
+
+	// Use this for initialization
+	void Start () {
+	}
+		
+	// Update is called once per frame
+	void Update () {
+	
+// Movement -----------------------------------------------------------------------------------------------------------------
+
+		// Jumping
+
+		if (Input.GetKeyDown (KeyCode.Space) && grounded) 
+		
+		{
+			GetComponent<Rigidbody2D> ().velocity = new Vector2 (GetComponent<Rigidbody2D> ().velocity.x, jumpHeight);
+		}
+	
+		// Double Jump
+
+		if (Input.GetKeyDown (KeyCode.Space) && !doubleJump && !grounded) 
+		
+		{
+			GetComponent<Rigidbody2D> ().velocity = new Vector2 (GetComponent<Rigidbody2D> ().velocity.x, jumpHeight);
+			doubleJump = true; 
+		}
+
+		if (grounded)
+			doubleJump = false;
+
+		// Horizontal Movement
+
+		moveVelocity = 0f; 
+	
+
+		if (Input.GetKey (KeyCode.D)) 
+		{
+			moveVelocity = moveSpeed;
+		}
+
+		if (Input.GetKey (KeyCode.A)) 
+		{
+			moveVelocity = -moveSpeed; 
+		}
+
+		GetComponent<Rigidbody2D> ().velocity = new Vector2 (moveVelocity, GetComponent<Rigidbody2D>().velocity.y);
+
+// Shooting -----------------------------------------------------------------------------------------------------------
+
+		if(Input.GetKeyDown(KeyCode.E))
+			{
+				Instantiate(sword, firePoint.position, firePoint.rotation);
+			}
+
+
+	}
+
+
+	void FixedUpdate() {
+
+		grounded = Physics2D.OverlapCircle (groundCheck.position, groundCheckRadius, whatIsGround); 
+			
+			
+
+		
+
+	}
+
+
+}
